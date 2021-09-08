241b8b4f0accf8d913b1d98dcdc87635f8c10e47
rfael5 <86300917+rfael5@users.noreply.github.com>
2021-09-08 19:10:57 -0300

    Update validacao.php

--- validacao.php
+++ validacao.php
@@ -1,5 +1,6 @@
 <?php
 
+    //Validando 198
     $numero = 198;
     if(isset($numero) && !empty($numero) && is_numeric($numero)){
         echo $numero.' existe, não está vazia, e é do tipo numérico.';
@@ -8,6 +9,8 @@
 
     };
     
+
+    //validando o array
     $endereco = [ 'nome' => 'Pedro Luiz', 'endereco' => 'Av Taquara', 'telefone' => '(51) 5151-51511' ];
     
     if(isset($endereco) && is_array($endereco) && isset($endereco['endereco'])){
@@ -17,6 +20,7 @@
 
     }
 
+    //validando o índice 'endereco' no array
     if(isset($endereco['endereco']) && !empty($endereco['endereco']) && is_string($endereco['endereco'])){
         echo 'Índice endereço existe, não está vazio e é do tipo string </br>';
         print_r($endereco['endereco']);
@@ -24,6 +28,7 @@
 
     }
 
+    //validando a string 'Romarinho'
     $nome = 'Romarinho';
     if(isset($nome) && !empty($nome) && is_string($nome)) {
         echo 'A variável $nome existe, não está vazia e é do tipo string:<br>'.$nome;
